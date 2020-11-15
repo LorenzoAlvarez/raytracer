50af1e66fe3a37954eb7e72cce01491acb2d927d
lorenzo.alvarez <lorenzo.alvarezlorenzo@gmail.com>
2020-11-15 20:15:15 +0100

    adding vec3 test and functions

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,7 +1,8 @@
 use std::ops::*;
+use std::f64;
 
 pub struct Vec3 {
-    pub e : [f64; 3]
+    pub e : [f64; 3],
 }
 
 impl Vec3 {
@@ -12,6 +13,14 @@ impl Vec3 {
 
     pub fn with_values(x : f64, y: f64, z: f64) -> Vec3{
         Vec3{ e : [x, y, z] }
+    }
+
+    fn length_squared(&self) -> f64 {
+        self.e[0]*self.e[0] + self.e[1]*self.e[1] + self.e[2]*self.e[2]
+    }
+
+    pub fn len(&self) -> f64 {
+        self.length_squared().sqrt()
     }
 
 }
@@ -59,6 +68,14 @@ impl AddAssign for Vec3 {
     }
 }
 
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Vec3){
+        self.e[0] -= other.e[0];
+        self.e[1] -= other.e[1];
+        self.e[2] -= other.e[2];
+    }
+}
+
 impl MulAssign<f64> for Vec3 {
     fn mul_assign(&mut self, t : f64){
         self.e[0] *= t;
@@ -69,9 +86,9 @@ impl MulAssign<f64> for Vec3 {
 
 impl DivAssign<f64> for Vec3 {
     fn div_assign(&mut self, t : f64){
+        self.e[0] /= t;
         self.e[1] /= t;
         self.e[2] /= t;
-        self.e[3] /= t;
     }
 }
 
@@ -95,7 +112,6 @@ impl IndexMut<usize> for Vec3 {
 #[cfg(test)]
 mod test_vec3 {
     use super::*;
-    
 
     #[test]
     fn test_new_empty(){
@@ -119,6 +135,70 @@ mod test_vec3 {
         assert_eq!(result.e, [-10.0, -15.0, -20.0]);
     }
 
+    #[test]
+    fn test_adding_and_assign(){
+        let mut vector = Vec3::with_values(10.0, 15.0, 20.0);
+        let vector2 = Vec3::with_values(10.0, 15.0, 20.0);
+        vector += vector2;
 
+        assert_eq!(vector.e, [20.0, 30.0, 40.0]);
+    }
+
+    #[test]
+    fn test_sub_and_assign(){
+        let mut vector = Vec3::with_values(10.0, 15.0, 20.0);
+        let vector2 = Vec3::with_values(10.0, 15.0, 20.0);
+        vector -= vector2;
+
+        assert_eq!(vector.e, [0.0, 0.0, 0.0]);
+    }
+
+    #[test]
+    fn test_mul_and_assign(){
+        let mut vector = Vec3::with_values(10.0, 15.0, 20.0);
+        vector *= 5.0;
+
+        assert_eq!(vector.e, [50.0, 75.0, 100.0]);
+    }
+
+    #[test]
+    fn test_div_and_assign(){
+        let mut vector = Vec3::with_values(10.0, 15.0, 20.0);
+        vector /= 5.0;
+
+        assert_eq!(vector.e, [2.0, 3.0, 4.0]);
+    }
+
+    #[test]
+    fn test_index(){
+        let vector = Vec3::with_values(10.0, 15.0, 20.0);
+        let x = vector[0];
+
+        assert_eq!(x, 10.0);
+    }
+
+    #[test]
+    fn test_mut_index(){
+        let mut vector = Vec3::with_values(10.0, 15.0, 20.0);
+        vector[0] = 0.0;
+
+        assert_eq!(vector[0], 0.0);
+    }
+
+    #[test]
+    fn test_length_square(){
+        let vector = Vec3::with_values(2.0, 2.0, 2.0);
+        let square_length = vector.length_squared();
+
+        assert_eq!(square_length, 12.0);
+    }
+
+    #[test]
+    fn test_length(){
+        let vector = Vec3::with_values(1.0, 1.0, 1.0);
+        let len = vector.len();
+
+        assert!(len - 1.7 < 0.1);
+    }
 
 }
